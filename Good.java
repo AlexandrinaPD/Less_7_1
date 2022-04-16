32e6c58ee0404100fab04e7660b05d0e0d929915
sandra <drinadp@gmail.com>
2022-04-16 16:34:52 +0200

    Shopping

---
+++ Good.java
@@ -0,0 +1,27 @@
+public class Good {
+
+
+    private String name;
+    private Integer cost;
+
+    public Good(String name, Integer cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+}
